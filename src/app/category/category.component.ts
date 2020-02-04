81ae110c9dc4e96efb3d4a59d484357bda6a310a
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-04 08:23:08 +0300

    Category komponenti oluşturuldu.

---
+++ src/app/category/category.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-category",
+  templateUrl: "./category.component.html",
+  styleUrls: ["./category.component.css"]
+})
+export class CategoryComponent implements OnInit {
+  constructor() {}
+  title = "Kategori Listesi";
+  categories: Category[] = [
+    { id: 1, name: "Elektronik" },
+    { id: 2, name: "Bilgisayar" },
+    { id: 3, name: "İletişim" },
+    { id: 4, name: "Müzik" }
+  ];
+  ngOnInit() {}
+}
