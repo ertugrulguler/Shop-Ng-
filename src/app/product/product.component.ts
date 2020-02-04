e0e8a6230bde4b4ab8aaa0b927c8a2ef003a65b3
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-04 08:23:44 +0300

    Product komponenti oluşturuldu.

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from "@angular/core";
+import { Product } from "./product";
+
+@Component({
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"]
+})
+export class ProductComponent implements OnInit {
+  constructor() {}
+
+  title = "Ürün Listesi";
+  products: Product[] = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 2500,
+      categoryId: 1,
+      description: "Asus zenBook",
+      imageUrl: "https://picsum.photos/200/200"
+    },
+    {
+      id: 2,
+      name: "Laptop",
+      price: 2500,
+      categoryId: 1,
+      description: "Lenova",
+      imageUrl: "https://picsum.photos/200/200"
+    },
+    {
+      id: 3,
+      name: "Mouse",
+      price: 50,
+      categoryId: 2,
+      description: "A4 Tech",
+      imageUrl: "https://picsum.photos/200/200"
+    },
+    {
+      id: 4,
+      name: "Telefon",
+      price: 5000,
+      categoryId: 3,
+      description: "İphone 7",
+      imageUrl: "https://picsum.photos/200/200"
+    },
+    {
+      id: 5,
+      name: "Telefon",
+      price: 6500,
+      categoryId: 3,
+      description: "İphone 7 Plus",
+      imageUrl: "https://picsum.photos/200/200"
+    }
+  ];
+
+  ngOnInit() {}
+}
