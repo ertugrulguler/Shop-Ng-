422d198b508d6f9f852d547549885dffc81a1bde
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-05 11:52:50 +0300

    Product servicede pipe işlemi yapılarak loglama ve hata yakalama işlemi yapıldı.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Injectable, ErrorHandler } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Product } from "../product/product";
-import { Observable } from "rxjs";
+import { Observable, throwError } from "rxjs";
+import { tap, catchError } from "rxjs/operators";
 
 @Injectable()
 export class ProductService {
@@ -10,6 +11,18 @@ export class ProductService {
   apiEndPoint = "http://localhost:3000/products";
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiEndPoint);
+    return this.http.get<Product[]>(this.apiEndPoint).pipe(
+      tap(response => console.log(JSON.stringify(response))),
+      catchError(this.handleError)
+    );
+  }
+  handleError(err: HttpErrorResponse) {
+    let errorMessage = "";
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = "Bir hata oluştu" + err.error.message;
+    } else {
+      errorMessage = "Sistemsel bir hata";
+    }
+    return throwError(errorMessage);
   }
 }
