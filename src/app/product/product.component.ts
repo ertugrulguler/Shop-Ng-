4f6fbc89a1b060da28f1276886c7f071aa966877
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-05 11:26:10 +0300

    Product service oluşturuldu.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,32 +1,31 @@
 import { Component, OnInit } from "@angular/core";
 import { Product } from "./product";
 import { AlertifyService } from "../services/alertify.service";
+import { ProductService } from "../services/product.service";
 // declare let alertify: any;
-import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-product",
   templateUrl: "./product.component.html",
-  styleUrls: ["./product.component.css"]
+  styleUrls: ["./product.component.css"],
+  providers: [ProductService]
 })
 export class ProductComponent implements OnInit {
   constructor(
     private alertifyService: AlertifyService,
-    private http: HttpClient
+    private productService: ProductService
   ) {}
 
   title = "Ürün Listesi";
   filterText = "";
   products: Product[];
-  apiEndPoint = "http://localhost:3000/products";
   showAlert(product) {
     // alertify.success("Ürünün adı :" + product);
     this.alertifyService.error("ürünün adı: " + product);
   }
   ngOnInit() {
-    this.http.get<Product[]>(this.apiEndPoint).subscribe(response => {
+    this.productService.getProducts().subscribe(response => {
       this.products = response;
-      console.log(this.products);
     });
   }
 }
