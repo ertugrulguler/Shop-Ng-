731257d0405709c92e81e5f800d52fd363b5be85
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-06 17:55:34 +0300

    Proje tasarımında değişiklikler yapıldı.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ProductComponent } from "./product/product.component";
+import { ProductAddForms1Service } from "./product/product-add-forms1.service";
 
 const routes: Routes = [
   { path: "products", component: ProductComponent },
+  { path: "product-add-1", component: ProductAddForms1Service },
+  { path: "product-add-2", component: ProductComponent },
   { path: "", redirectTo: "products", pathMatch: "full" },
   { path: "products/category/:categoryId", component: ProductComponent }
 ];
