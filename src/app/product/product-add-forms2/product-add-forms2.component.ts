35dcbcb271caea30444a667ba7c0aad9a68f12ce
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-07 12:04:02 +0300

    Reactif olarak ürün ekleme sayfası tamamlandı.

--- src/app/product/product-add-forms2/product-add-forms2.component.ts
+++ src/app/product/product-add-forms2/product-add-forms2.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Product } from "../product";
+import { CategoryService } from "src/app/services/category.service";
+import { Category } from "src/app/category/category";
+import { ProductService } from "src/app/services/product.service";
+import { AlertifyService } from "src/app/services/alertify.service";
 
 @Component({
   selector: "app-product-add-forms2",
   templateUrl: "./product-add-forms2.component.html",
-  styleUrls: ["./product-add-forms2.component.css"]
+  styleUrls: ["./product-add-forms2.component.css"],
+  providers: [CategoryService, ProductService, AlertifyService]
 })
 export class ProductAddForms2Component implements OnInit {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService,
+    private productService: ProductService,
+    private alertifyService: AlertifyService
+  ) {}
 
   productAddForm: FormGroup;
   product: Product = new Product();
+  categories: Category[];
 
   createProduct() {
     this.productAddForm = this.formBuilder.group({
@@ -23,9 +34,19 @@ export class ProductAddForms2Component implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createProduct();
+    this.categoryService.getCategories().subscribe(response => {
+      this.categories = response;
+    });
+  }
 
   add() {
-    this.product = Object.assign({}, this.productAddForm.value);
+    if (this.productAddForm.valid) {
+      this.product = Object.assign({}, this.productAddForm.value);
+    }
+    this.productService.createProduct(this.product).subscribe(response => {
+      this.alertifyService.success(response.name + " başarıyla eklendi.");
+    });
   }
 }
