63d5fd10419df1483ef32cedf087c926550386e7
Ertuğrul Güler <ertglr.61@gmail.com>
2020-02-08 19:38:07 +0300

    Login metodları yanlış yerde tanımlanmıştı. AppComponent.ts klasörüne taşındı.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AccountService } from "./services/account.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'shop';
+  title = "shop";
+  constructor(private accountService: AccountService) {}
+
+  isLoggedin() {
+    return this.accountService.isLoggedIn();
+  }
+
+  isLogout() {
+    return this.accountService.logOut();
+  }
 }
